a01f73c16d85252c72d1a63c63d173e1d107f356
lucastepper <lucas.tepper.91@gmail.com>
2021-11-22 11:00:05 +0100

    changed convolution to dot prod

--- kernel_extraction/src/lib.rs
+++ kernel_extraction/src/lib.rs
@@ -3,17 +3,21 @@ use numpy::{IntoPyArray, PyArray};
 use ndarray::Dim;
 
 
-fn _g_method_second_cpu(vv_corr: &[f64], xdu_corr: &[f64], dt: f64, trunc: usize) -> Vec<f64> {
+fn dot_prod(a: &[f64], b: &[f64]) -> f64 {
+    assert_eq!(a.len(), b.len());
+    a.iter().zip(b.iter()).map(|v| v.0 * v.1).sum()
+}
+
+fn _g_method_second_cpu(vv_corr_rev: &[f64], xdu_corr: &[f64], dt: f64, trunc: usize) -> Vec<f64> {
+    let len =  vv_corr_rev.len();
+    let mut convolution: f64;
     let mut kernel = vec![0.; trunc];
     for i in 1..trunc {
-        let mut convolution = 0.;
-        for j in 1..i {
-            convolution += kernel[j] * vv_corr[i - j];
-        }
-        kernel[i] = 2. / vv_corr[0] * (
-            (xdu_corr[i] - vv_corr[i] * xdu_corr[0] / vv_corr[0]) / dt
-            - convolution
-        );
+        convolution = dot_prod(&kernel[1..i], &vv_corr_rev[(len - i)..(len - 1)]);
+        kernel[i] = 2. / vv_corr_rev[len - 1] * ((
+            xdu_corr[i]
+            - vv_corr_rev[len - 1 - i] * xdu_corr[0] / vv_corr_rev[len - 1]
+        ) / dt - convolution);
     }
     kernel
 }
@@ -30,7 +34,12 @@ fn kernel_extraction(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
         dt: f64,
         trunc: usize,
     ) -> &'py PyArray<f64, Dim<[usize; 1]>> {
-        _g_method_second_cpu(&vv_corr, &xdu_corr, dt, trunc).into_pyarray(py)
+        // flipping vv_corr once and compution convolution as dot prod is faster
+        let vv_corr_rev: Vec<f64> = vv_corr.iter()
+                                           .rev()
+                                           .map(|x| *x)
+                                           .collect();
+        _g_method_second_cpu(&vv_corr_rev, &xdu_corr, dt, trunc).into_pyarray(py)
     }
     Ok(())
 }
