630c081550d5d651a0ac4ade41a120c71f4e34a9
lucastepper <lucas.tepper.91@gmail.com>
2021-11-22 11:08:45 +0100

    SIMD and parallel dot prods

--- kernel_extraction/build_n_test.sh
+++ kernel_extraction/build_n_test.sh
@@ -5,20 +5,20 @@ test_script="test_g_method_second_cpu.py"
 time_script="time_g_method_second_cpu.py"
 
 # find out if building debug or release
-if ! ([ "$1" = "debug" ] || [ "$1" = "release" ]); then 
+if ! ([ "$1" = "debug" ] || [ "$1" = "release" ]); then
 	build_mode="debug"
 else
 	build_mode=$1
 fi
 echo "Running in: $build_mode mode"
 
-# if build succesfull, link .so to projects top library 
+# if build succesfull, link .so to projects top library
 # for debug, exec test script
 # for release exec the timing script
 if [ "$build_mode" = "release" ]; then
-	RUSTFLAGS="-C opt-level=3 -C debuginfo=0 -C target-cpu=native" cargo build --release
+	RUSTFLAGS="-C opt-level=3 -C debuginfo=0 -C target-cpu=native" cargo +nightly build --release
 else
-	cargo build
+	cargo +nightly build
 fi
 if [ $? -eq 0 ]; then
 	ln -sf target/${build_mode}/lib${package_name}.so ${package_name}.so
