1006d974623e0fb27077d70c731b914639f65dc1
lucastepper <lucas.tepper.91@gmail.com>
2021-11-22 10:34:31 +0100

    test with ref data

--- kernel_extraction/test/test_g_method_second_cpu.py
+++ kernel_extraction/test/test_g_method_second_cpu.py
@@ -1,1 +1,31 @@
-import kernel_extraction; print(kernel_extraction.__file__)
+import numpy as np
+import kernel_extraction
+
+
+def _calc_G_method(kernel_i, xu_cf, v_acf, dt, trunc, verbose=False):
+    """Compute the integral over the kernel."""
+
+    prefac = 2.0 / v_acf[0]
+    for i in range(1, trunc):
+        kernel_i[i] = prefac * (
+            (xu_cf[i] - v_acf[i] * xu_cf[0] / v_acf[0]) / dt
+            - np.sum(kernel_i[1:i] * v_acf[1:i][::-1])
+        )
+        if verbose and i % 10000 == 0:
+            print("progress: ", round(i / trunc * 100, 3), "%")
+
+
+def main():
+    dt = 0.01
+    trunc = 10
+    vv_corr = np.load("test/vv_corr_ref.npy")[:10]
+    xdu_corr = np.load("test/xdu_corr_ref.npy")[:10]
+    kernel_i_ref = np.zeros(trunc)
+    _calc_G_method(kernel_i_ref, xdu_corr, vv_corr, dt, trunc)
+    kernel_i_test = kernel_extraction.g_method_second_cpu(vv_corr, xdu_corr, dt, trunc)
+    np.testing.assert_allclose(kernel_i_ref, kernel_i_test)
+    print("Test successful!")
+
+
+if __name__ == "__main__":
+    main()
--- kernel_extraction/test/time_g_method_second_cpu.py
+++ kernel_extraction/test/time_g_method_second_cpu.py
@@ -1,1 +1,46 @@
-import kernel_extraction; print(kernel_extraction.__file__)
+from time import time
+import numpy as np
+import kernel_extraction
+import matplotlib.pyplot as plt
+
+
+def main():
+    dt = 0.001
+    trunc_max = int(1e6)
+    kernel_ref = np.load("test/kernel_ref.npy")
+    xdu_corr_ref = np.load("test/xdu_corr_ref.npy")
+    vv_corr_ref = np.load("test/vv_corr_ref.npy")
+
+    truncs = []
+    times = []
+    for trunc in [1, 2, 3, 4, 5, 5.2, 5.3, 5.5, np.log10(trunc_max)]:
+        trunc = int(10 ** trunc)
+        t1 = time()
+        kernel_i_test = kernel_extraction.g_method_second_cpu(
+            vv_corr_ref, xdu_corr_ref, dt, trunc
+        )
+        time_run = time() - t1
+        times.append(time_run)
+        print(f"{trunc=}, {time_run=} s")
+        truncs.append(trunc)
+
+    plt.plot(truncs, times, marker="x")
+    plt.xlabel("trunc")
+    plt.ylabel("time [s]")
+    for i in range(1, 3):
+        plt.annotate(str(round(times[-i], 1)), (truncs[-i], times[-i]))
+    plt.savefig("timings.pdf")
+    plt.clf()
+
+    plt.plot(
+        np.arange(trunc)[:trunc] * dt, np.gradient(kernel_i_test, dt), label="test"
+    )
+    plt.plot(np.arange(trunc) * dt, kernel_ref[:trunc], label="ref", alpha=0.6)
+    plt.semilogx()
+    plt.legend()
+    plt.savefig("kernel_compare.pdf")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
